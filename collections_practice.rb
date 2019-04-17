172076a7dc31f4f0b78b89bd6a39127e51c81a40
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-17 19:49:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,7 +15,12 @@ end
 
 def sort_array_char_count(array)
   array.sort do |a, b|
-    a.length <=> b.length
+  if a.length == b.length
+    0
+  elsif a.length < b.length
+    -1
+  elsif a.length > b.length
+    1
+  end
 end
-array.sort
 end
